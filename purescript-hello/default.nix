a495b161f8391b9581f1f48160bb254162b35fc0
Nicholas Kariniemi <nicholas.kariniemi@reaktor.com>
2017-06-02 18:28:57 -0400

    Purescript hello world

---
+++ purescript-hello/default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import ./pkgs.nix }:
+
+pkgs.stdenv.mkDerivation {
+  name = "purescript-hello";
+  src = ./.;
+  buildInputs = [
+    pkgs.purescript
+    pkgs.nodejs-7_x
+    pkgs.nodePackages.bower
+    pkgs.git
+  ];
+  buildPhase = ''
+    HOME=. npm install
+    export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+    HOME=. bower update --force-latest
+    npm run build
+  '';
+  checkPhase = ''
+    npm run test
+  '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r output/hello.js node_modules $out/
+    echo "#!${pkgs.bash}/bin/bash" > $out/bin/hello-purescript
+    echo "${pkgs.nodejs}/bin/node $out/hello.js" >> $out/bin/hello-purescript
+    chmod +x $out/bin/hello-purescript
+  '';
+}
